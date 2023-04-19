0102b22c4e06411a616aad3ebb121b0d37fe4141
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-19 12:34:35 +0200

    Add Milestone 0

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,26 @@ export default {
     return {
       store
     }
+  },
+  methods: {
+    callApi(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response.data.results)
+        })
+        .catch(err => {
+          console.log(err);
+          console.log(err.message);
+        })
+    }
+  },
+  mounted() {
+    this.callApi(store.API_URL)
   }
 }
 </script>
 
-<template>
+<template></template>
 
-</template>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {reactive } from 'vue'
 
 const store = reactive({
     searchText: '',
+    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=ffce8ca545654f470f90e4967c10b588&language=en-US&query=f'
 })
 
 export { store }
