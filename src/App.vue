afe266370d85c4afcd163ce6fbd8fa6e02a0fa4f
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-19 13:34:56 +0200

    add filter movie

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,54 @@
 import { store } from './store'
 import axios from 'axios'
 export default {
-  data() {
-    return {
-      store
-    }
-  },
-  methods: {
-    callApi(url) {
-      axios
-        .get(url)
-        .then(response => {
-          console.log(response.data.results)
-        })
-        .catch(err => {
-          console.log(err);
-          console.log(err.message);
-        })
-    }
-  },
-  mounted() {
-    this.callApi(store.API_URL)
-  }
+   data() {
+      return {
+         store
+      }
+   },
+   methods: {
+      callApi(url) {
+         axios
+            .get(url)
+            .then(response => {
+               console.log(response.data.results)
+            })
+            .catch(err => {
+               console.log(err);
+               console.log(err.message);
+               store.movies = response.data.results
+            })
+      },
+      filterMovies() {
+         if (store.searchText.length > 0) {
+            this.callApi(store.API_URL + store.searchText)
+         }
+      }
+   },
+   mounted() {
+      this.callApi(store.API_URL)
+   }
 }
 </script>
 
-<template></template>
+<template>
+   <!-- Header -->
+   <header class="p-3">
+      <h1>Boolflix</h1>
+      <input v-model="store.searchText" @keyup.enter="filterMovies()">
+      <button @click="filterMovies()" class="mx-2">Search</button>
+   </header>
+   <!-- Main -->
+   <main>
+      <ul v-if="store.movies">
+         <li v-for="movie in store.movies">
+            <h3>{{ movie.title }}</h3>
+            <p>{{ movie.original_title }}</p>
+            <p>{{ movie.original_language }}</p>
+            <p>{{ movie.vote_average }}</p>
+         </li>
+      </ul>
+   </main>
+</template>
 
 <style scoped></style>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import {reactive } from 'vue'
+import { reactive } from 'vue'
 
 const store = reactive({
     searchText: '',
-    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=ffce8ca545654f470f90e4967c10b588&language=en-US&query=f'
+    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=ffce8ca545654f470f90e4967c10b588&language=en-US&query=',
+    movies: null
 })
 
 export { store }
